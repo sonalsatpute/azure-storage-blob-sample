3cb4dae35bb67b3c3339a39df2b99945c9716287
sonalsatpute <sonal.satpute@gmail.com>
2017-10-24 15:23:52 +0530

    upload blob

--- azure-storage-blob-sample/BlobStorage/Program.cs
+++ azure-storage-blob-sample/BlobStorage/Program.cs
@@ -1,9 +1,29 @@
-﻿namespace BlobStorage
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.IO;
+
+namespace BlobStorage
 {
   class Program
   {
     static void Main(string[] args)
     {
+    }
+
+    static void Upload()
+    {
+      var connectionString = System.Configuration.ConfigurationManager.AppSettings["StorageConnectionString"];
+      CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
+      CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+      CloudBlobContainer blobContainer = blobClient.GetContainerReference("testcontainer");
+      blobContainer.CreateIfNotExists();
+
+      CloudBlockBlob blockBlob = blobContainer.GetBlockBlobReference("532_OD_Changes.pdf");
+
+      using (var fileStream = File.OpenRead(@"D:\temp\532_OD_Changes.pdf"))
+      {
+        blockBlob.UploadFromStream(fileStream);
+      }
     }
   }
 }
